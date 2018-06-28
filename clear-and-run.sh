4cb2178a356e0fa0909f4399126085af10ecb9d5
senalkularatne <senal.kularatne@ebth.com>
2018-06-28 16:36:42 -0400

    Automatically open dag graph view page on Google Chrome

--- clear-and-run.sh
+++ clear-and-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Provide the dag_id of the dag you want to clear and rerun: "
 read dag_id
-airflow clear -u -d -c -sd ~/airflow/dags $dag_id
-echo "--------------------------------------------- "
-echo "---------------- Cleared Dag ---------------- "
-echo "--------------------------------------------- "
+airflow clear -u -d -c -sd ~/airflow/dags 
+echo "---------------------------------------------"
+echo "---------------- Cleared Dag ----------------"
+echo "---------------------------------------------"
 airflow trigger_dag s3_a_json_sensor
-echo "--------------------------------------------- "
-echo "-------------- Dag is Running -------------- "
-echo "--------------------------------------------- "
+echo "---------------------------------------------"
+echo "--------------- Dag is Running --------------"
+echo "---------------------------------------------"
+/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8080/admin/airflow/graph?dag_id=$dag_id"
+
